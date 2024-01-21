b1aa40d70b9fd5774b14733370f0a93b543eb3f5
DenZ1971 <d.zapekin@gmail.com>
2024-01-21 17:09:24 +0100

    first commit

---
+++ success.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Ваш заголовок страницы и другие мета-теги -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="message">Ďakujeme za Vašu objednávku!</div>
+
+    <!-- Дополнительный контент страницы, если необходимо -->
+</body>
+</html>
+
